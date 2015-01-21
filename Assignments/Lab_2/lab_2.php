de6da916d0f8c24f7d69805deee3ed36feeb592f
unknown <rjb06211@CLK310-14.vtc.vsc.edu>
2015-01-20 20:27:16 -0500

    lab 2 is done

--- Assignments/Lab_2/lab_2.php
+++ Assignments/Lab_2/lab_2.php
@@ -6,7 +6,7 @@
  * This lab focuses on your understanding of arithmatic.
  *
  * @version 1.0
- * @author YOUR_NAME <YOUR_EMAIL_ADDRESS@vtc.edi>
+ * @author Ryan Bates <rbates@vtc.edu>
  * @since 20150120
  */
 
@@ -23,29 +23,47 @@ define('GRAVITY', 9.8);
 
 function truncateFloat($float_value)
 {
-
+$truncate = (int)($float_value*100)/100;
+echo $truncate;
 }
 
 /**
- * @param $degrees_f
+ * Outputs to the console farenheit to Kelvin
+ *
+ * @param $degrees_f degrees in farenheit
  */
 function farenheit2Kelvin($degrees_f)
 {
-
+$degrees_k = ($degrees_f - 32) * 5/9 + 273.15;
+echo $degrees_k;
 }
 
 /**
- * @param $area
+ * Outputs to the console dodecahedron Volume
+ *
+ * @param $area area of object
  */
 function dodecahedronVolume($area)
 {
-
+$volume = ((15 + 7*sqrt(5)) / 4)*pow($area,3);
+echo $volume;
 }
 
 /**
- * @param $height
+ * Outputs to the console impact velocity
+ *
+ * @param $height height of falling object 
  */
 function impactVelocity($height)
 {
-
+$velocity = sqrt(2*GRAVITY*$height);
+echo $velocity;
 }
+
+truncateFloat(30.1231231231);
+echo "<br>";
+farenheit2Kelvin(30);
+echo "<br>";
+dodecahedronVolume(10);
+echo "<br>";
+impactVelocity(50);
